c7e1d83b0049d1425d3b0e4e8812086b5b240e81
lvivduncan <lvivduncan@gmail.com>
2020-10-27 09:55:29 +0200

    home desktop finish

--- js/init.js
+++ js/init.js
@@ -3,10 +3,28 @@
 $('#carousel').owlCarousel({
     animateOut: 'slideOutDown',
     animateIn: 'flipInX',
-    items:1,
-    margin:30,
-    stagePadding:30,
-    smartSpeed:450,
+    items: 1,
+    margin: 30,
+    stagePadding: 30,
+    smartSpeed: 450,
     dots: true,
     autoplay: true
 });
+
+
+
+$('#testimonials').owlCarousel({
+    loop: true,
+    margin: 40,
+    dots: true,
+    autoplay: true,
+    items: 2,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        }
+    }
+})
